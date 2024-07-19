d0c7874ed4daab1d5b280f0a261fc9193c412295
ERFAN.NB <naboureh.1385@gmail.com>
2024-07-19 22:09:39 +0330

    metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./../styles/globals.css";
 import AOSInit from "@/utils/aos";
+import { Metadata } from "next";
+
+// export const metadata = {
+//   title: "NFT by ERFAN",
+//   description: "NFT Project created by ERFAN.NB",
+// };
 
 export const metadata = {
   title: "NFT by ERFAN",
-  description: "NFT Project created by ERFAN.NB",
+  openGraph: {
+    title: "NFT",
+    description: "NFT Project created by ERFAN.NB",
+  },
 };
 
 export default function RootLayout({ children }) {
