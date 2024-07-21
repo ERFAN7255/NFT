90cd4925b73de632478b697a73340793839b23c4
ERFAN.NB <naboureh.1385@gmail.com>
2024-07-21 20:05:29 +0330

    create notFound page

--- src/app/loading.js
+++ src/app/loading.js
@@ -3,10 +3,11 @@ import styles from "@/styles/loading.module.css";
 
 function loading() {
   return (
-    <div class={styles.three_body}>
-      <div class={styles.three_body__dot}></div>
-      <div class={styles.three_body__dot}></div>
-      <div class={styles.three_body__dot}></div>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div class={styles.loader}>
+        <span class={styles.loader_text}>loading</span>
+        <span class={styles.load}></span>
+      </div>
     </div>
   );
 }
