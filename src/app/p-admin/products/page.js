a3622fbc072b83a0d4f9db420fc215ef212f6d1e
ERFAN.NB <naboureh.1385@gmail.com>
2024-07-25 20:08:08 +0330

    create and design tickets p-admin page

--- src/app/p-admin/products/page.js
+++ src/app/p-admin/products/page.js
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import React from "react";
+import swal from "sweetalert";
 
 function page() {
   return (
--- src/app/p-admin/tickets/page.js
+++ src/app/p-admin/tickets/page.js
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import React from "react";
 import swal from "sweetalert";
 
@@ -9,14 +8,14 @@ function page() {
       <table className="border-2 border-black">
         <thead>
           <tr>
-            <th className="border-2 lg:text-xl xl:text-2xl border-purple-600 text-center p-4 md:p-2 text-purple-400">
-              کاربر
+            <th className="border-2 lg:text-xl xl:text-2xl border-purple-600 text-center p-2 md:p-2 text-purple-400">
+              تعداد
             </th>
             <th className="border-2 lg:text-xl xl:text-2xl border-purple-600 text-center text-purple-400">
               نام
             </th>
-            <th className="border-2 lg:text-xl xl:text-2xl border-purple-600 p-4 md:p-2 text-center text-purple-400">
-              شماره تلفن
+            <th className="border-2 lg:text-xl xl:text-2xl border-purple-600 p-4 md:p-2 text-center text-blue-400">
+              پیام
             </th>
             <th className="border-2 lg:text-xl xl:text-2xl border-purple-600 p-4 md:p-2 text-center text-red-500">
               حذف
@@ -25,14 +24,25 @@ function page() {
         </thead>
         <tbody>
           <tr>
-            <td className="border-2 lg:text-xl xl:text-2xl border-purple-600 p-2 md:p-2 text-center text-white">
+            <td className="border-2 lg:text-xl xl:text-2xl border-purple-600 md:p-2 text-center text-white">
               1
             </td>
             <td className="border-2 lg:text-xl xl:text-2xl border-purple-600 p-2 md:p-2 text-center text-white">
               ERFAN725
             </td>
             <td className="border-2 lg:text-xl xl:text-2xl border-purple-600 p-3 md:p-2 text-center text-white">
-              09052083383
+              <button
+                className="bg-blue-500 rounded-md text-white p-2 md:p-3 md:px-3 lg:px-5 lg:p-2"
+                onClick={() =>
+                  swal({
+                    title: "صبر کن بچه جان سایت هنوز کامل نشده 🤨",
+                    icon: "warning",
+                    buttons: "چشم",
+                  })
+                }
+              >
+                دیدن پیام
+              </button>
             </td>
             <td className="border-2 lg:text-xl xl:text-2xl border-purple-600 p-2 md:p-2 text-center">
               <button
