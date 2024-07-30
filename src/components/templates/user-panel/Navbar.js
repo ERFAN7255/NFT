58003cbb652f3b4bf84bbae266ffd0e8935997fe
ERFAN.NB <naboureh.1385@gmail.com>
2024-07-30 17:13:02 +0330

    edit navbar my-account

--- src/components/templates/user-panel/Navbar.js
+++ src/components/templates/user-panel/Navbar.js
@@ -101,6 +101,26 @@ function Navbar() {
                 </svg>
                 جزییات حساب
               </Link>
+              <Link
+                href={"/"}
+                className="flex gap-2 border-2 border-blue-400 p-2 rounded-xl text-blue-400"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                  />
+                </svg>
+                برگشت به صفحه اصلی
+              </Link>
               <li className="flex gap-2 border-2 border-red-600 p-2 rounded-xl text-red-600">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -222,6 +242,26 @@ function Navbar() {
                 />
               </svg>
               جزییات حساب
+            </Link>
+            <Link
+              href={"/"}
+              className="flex gap-2 border-2 border-blue-400 p-2 rounded-xl text-blue-400"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                />
+              </svg>
+              برگشت به صفحه اصلی
             </Link>
             <li className="flex gap-2 border-2 border-red-600 p-2 rounded-xl text-red-600">
               <svg
